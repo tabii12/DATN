378e96c43e2ee8b9599e264d4c19209dedaee9ba
tabii <tabibito2k512@gmail.com>
2026-02-27 08:42:21 +0700

    fix(tours-page): add 'use client' to resolve useSearchParams build error

--- app/(user)/tours/[slug]/page.tsx
+++ app/(user)/tours/[slug]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import HotelDetailPage from "../../components/hotels/HotelDetailPage";
 
 // Page nhận params từ Next.js App Router → truyền slug xuống client component
--- app/(user)/tours/page.tsx
+++ app/(user)/tours/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import HotelListingPage from "../components/hotels/HotelListingPage";
 
 interface Props {
