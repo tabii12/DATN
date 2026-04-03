77fb608fa0b74b66dc09d313c7da47770b417dc7
tabii <tabibito2k512@gmail.com>
2026-04-03 15:08:56 +0700

    fix: overflow

--- app/(user)/components/layout/Header.tsx
+++ app/(user)/components/layout/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
   return (
     <header className="w-full bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4">
-        <div className="flex items-center justify-between h-14 overflow-hidden">
+        <div className="flex items-center justify-between h-14">
           {/* LOGO */}
           <Link href="/" className="flex items-center gap-0.5 shrink-0">
             <img src="https://res.cloudinary.com/dmv7ymjxf/image/upload/v1775203365/logo-removebg-preview_iyvg9e.png" alt="" width={"200px"}/>
